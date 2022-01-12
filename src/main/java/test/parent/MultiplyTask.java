e2bb873863699ccfbaa8c8eaae6933c15a5444c2
nikolanguen <43477149+nikolanguen@users.noreply.github.com>
2022-01-12 14:56:47 +0200

    Update MultiplyTask.java

--- src/main/java/test/parent/MultiplyTask.java
+++ src/main/java/test/parent/MultiplyTask.java
@@ -1,9 +1,11 @@
 package test.parent;
 
 public class MultiplyTask {
-    public static void multiply(int x, int y){
-        while(true){
-            System.out.println("asd");
+    public static int multiply(int x, int y){
+        boolean flag = true;
+        while(flag){
+            System.out.println("pet");
         }
+        return x * y;
     }
 }
